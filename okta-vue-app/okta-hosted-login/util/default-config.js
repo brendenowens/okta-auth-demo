71ffccc5ef73c2ed68fa2a77b87a5b0cee5fc09e
Brenden Owens <bowens3@nd.edu>
2019-01-25 17:07:56 -0500

    adding working vue.js app with auth to okta

---
+++ okta-vue-app/okta-hosted-login/util/default-config.js
@@ -0,0 +1,11 @@
+export default {
+  oidc: {
+    clientId: '0oaj405d7paKw2ssA0h7',
+    issuer: 'https://dev-738834.oktapreview.com/oauth2/default',
+    redirectUri: 'http://localhost:8080/implicit/callback',
+    scope: 'openid profile email'
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages'
+  }
+}
